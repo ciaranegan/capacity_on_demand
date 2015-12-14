b3c7639d80d1163a35c32ac4528f3a35feb5950f
Ciaran Egan <ciaranegan93@gmail.com>
2015-12-14 17:51:39 +0000

    topology dir & gitignore

---
+++ topologies/topology.py
@@ -0,0 +1,37 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import irange, dumpNodeConnections
+from mininet.log import setLogLevel
+
+class LinearTopo(Topo):
+
+	def __init__(self, num_switches=3, **opts):
+		super(LinearTopo, self).__init__(**opts)
+
+		self.num_switches = num_switches
+
+		previous = None
+		for i in range(1, num_switches):
+			# Create and connect a host and a switch
+			host = self.addHost('h%s' % i)
+			switch = self.addSwitch('s%s' % i)
+			self.addLink(host, switch)
+
+			if previous:
+				# Connect the switch to the previous switch
+				self.addLink(switch, previous)
+			previous = switch1
+
+
+def test():
+	topo = LinearTopo(k=4)
+	net = Mininet(topo)
+	net.start()
+	dumpNodeConnections(net.hosts)
+	net.pingAll()
+	net.stop()
+
+
+if __name__ == '__main__':
+	setLogLevel('info')
+	test()
