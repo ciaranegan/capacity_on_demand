73837ae1fbe79c0253b2d8aee665e46d032526ea
Ciaran Egan <ciaranegan93@gmail.com>
2015-12-15 10:30:14 +0000

    test file

--- topologies/topology.py
+++ topologies/topology.py
@@ -24,7 +24,7 @@ class LinearTopo(Topo):
 
 
 def test():
-	topo = LinearTopo(k=4)
+	topo = LinearTopo()
 	net = Mininet(topo)
 	net.start()
 	dumpNodeConnections(net.hosts)
@@ -33,5 +33,5 @@ def test():
 
 
 if __name__ == '__main__':
-	setLogLevel('info')
+	setLogLevel('debug')
 	test()
