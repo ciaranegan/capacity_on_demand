7d66c0f1860d8bacc6c5014e89a832162e73b352
Ciaran Egan <ciaranegan93@gmail.com>
2016-02-24 16:26:45 +0000

    Moved models to separate file. Style cleanup

---
+++ ryu/ryu/app/qos/models.py
@@ -0,0 +1,65 @@
+from enum import Enum
+from sqlalchemy import (Column, ForeignKey, Integer, String, create_engine,
+                        Float, Enum)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+INGRESS = "ingress"
+EGRESS = "egress"
+
+
+class QoSReservation(Base):
+    """
+    Class to represent a bandwidth allocation.
+    """
+    __tablename__ = 'reservation'
+
+    id = Column(Integer, primary_key=True, auto_increment=True)
+    src = Column(String)
+    dst = Column(String)
+    bw = Column(Float)
+    switches = relationship("QoSSwitch")
+
+
+class QoSPortReservation:
+    """
+    Class to represent an allocation for a specific port.
+    """
+    __tablename__ = "port_reservation"
+
+    id = Column(Integer, primary_key=True, auto_increment=True)
+    port = Column(Integer, ForeignKey("port.id"))
+    bw = Column(Integer)
+    reservation = Column(Integer, ForeignKey("reservation.id"))
+
+
+class QoSSwitch:
+    """
+    Class to represent a switch.
+    """
+    __tablename__ = "switch"
+
+    TYPE = Enum(INGRESS, EGRESS)
+
+    dpid = Column(Integer, primary_key=True)
+    switch_type = Column(ENUM(*TYPE), default=TYPE.EGRESS)
+    ports = relationship("QoSPort")
+
+
+class QoSPort:
+    """
+    Class to represent a port.
+    """
+    __tablename__ = "port"
+
+    TYPE = Enum(INGRESS, EGRESS)
+
+    id = Column(Integer, primary_key=True, auto_increment=True)
+    port_no = Column(Integer)
+    src_switch = Column(Integer, ForeignKey("switch.dpid"))
+    dst_switch = Column(Integer, ForeignKey("switch.dpid"))
+    total_bw = Column(Float)
+    port_type = Column(Enum(*TYPE), default=TYPE.EGRESS)
+    reservations = relationship("QoSPortReservation")
--- ryu/ryu/app/qos/qos_tracker.py
+++ ryu/ryu/app/qos/qos_tracker.py
@@ -1,75 +1,13 @@
-from enum import Enum
-from sqlalchemy import (Column, ForeignKey, Integer, String, create_engine,
-						Float)
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-
-Base = declarative_base()
-
-INGRESS = "ingress"
-EGRESS = "egress"
+from models import QoSSwitch, QoSPort
 
 class QoSTracker:
 
-	def __init__(self):
-		self.reservations = []
-		self.topology = None
+    def __init__(self):
+        self.reservations = []
+        self.topology = None
 
-	def add_reservation(self, src, dst, bw):
-		self.reservations.append(QoSReservation(src, dst, bw))
-
-	def remove_reservation(self, res):
-		self.reservations.remove(res)
-
-
-class QoSReservation(Base):
-	"""
-	Class to represent a bandwidth allocation.
-	"""
-	__tablename__ = 'reservation'
-
-	id = Column(Integer, primary_key=True, auto_increment=True)
-	src = Column(String)
-	dst = Column(String)
-	bw = Column(Float)
-	switches = relationship("QoSSwitch")
-
-class QoSPortReservation:
-	"""
-	Class to represent an allocation for a specific port.
-	"""
-	__tablename__ = "port_reservation"
-
-	id = Column(Integer, primary_key=True, auto_increment=True)
-	port = Column(Integer, ForeignKey("port.id"))
-	bw = Column(Integer)
-	reservation = Column(Integer, ForeignKey("reservation.id"))
-
-
-class QoSSwitch:
-	"""
-	Class to represent a switch.
-	"""
-	__tablename__ = "switch"
-
-	TYPE = Enum(INGRESS, EGRESS)
-
-	dpid = Column(Integer, primary_key=True)
-	switch_type = Column(ENUM(*TYPE), default=TYPE.EGRESS)
-	ports = relationship("QoSPort")
-
-
-class QoSPort:
-	"""
-	Class to represent a port.
-	"""
-	TYPE = Enum(INGRESS, EGRESS)
-
-	id = Column(Integer, primary_key=True, auto_increment=True)
-	port_no = Column(Integer)
-	switch = Column(Integer, ForeignKey("switch.id"))
-	total_bw = Column(Float)
-	port_type = Column(Enum(*TYPE), default=TYPE.EGRESS)
-	reservations = relationship("QoSPortReservation")
-
-
+    def add_switch(self, dpid, ports=None, switch_type=QoSSwitch.TYPE.EGRESS):
+        switch = QoSSwitch(dpid=dpid, switch_type=switch_type)
+        for port in ports:
+            p = QoSPort(port_no=port["port_no"], switch=switch.dpid, total_bw=port["bw"],
+                        port_type=QoSPort.TYPE.EGRESS)
