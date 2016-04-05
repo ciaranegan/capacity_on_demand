2b04c0d108ff71cbcacc92a228e12fc68d1a9492
Ciaran Egan <ciaranegan93@gmail.com>
2016-04-05 17:58:33 +0100

    Added switch to the middle of 2x4 topo

--- topologies/2_switches_4_hosts.py
+++ topologies/2_switches_4_hosts.py
@@ -20,12 +20,16 @@ class Switches4HostsTopo(Topo):
 
         switch0 = self.addSwitch('s0', dpid='0000000000000010')
         switch1 = self.addSwitch('s1', dpid='0000000000000020')
+        switch2 = self.addSwitch('s2', dpid='0000000000000030')
 
         self.addLink(host0, switch0, bw=100)
         self.addLink(host1, switch0, bw=100)
+
         self.addLink(host2, switch1, bw=100)
         self.addLink(host3, switch1, bw=100)
-        self.addLink(switch0, switch1, bw=50)
+
+        self.addLink(switch0, switch2, bw=50)
+        self.addLink(switch1, switch2, bw=50)
 
 topos = {'2switch4host': (lambda: Switches4HostsTopo())}
 
