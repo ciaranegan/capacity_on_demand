9b6ba23d90c09bd9019ea7c97beded4704d6e479
Ciaran Egan <ciaranegan93@gmail.com>
2016-04-17 16:03:52 +0100

    Added shell scripts to retrieve switch info

---
+++ bin/get_queues.sh
@@ -0,0 +1,3 @@
+curl -X GET http://localhost:8080/qos/queue/0000000000000010
+curl -X GET http://localhost:8080/qos/queue/0000000000000020
+curl -X GET http://localhost:8080/qos/queue/0000000000000030
