ffcbdd115437947979c8d6695b5bc9da955448d8
Ciaran Egan <ciaranegan93@gmail.com>
2016-04-24 11:41:28 -0700

    another index fix

--- ryu/ryu/app/new_qos/qos_tracker.py
+++ ryu/ryu/app/new_qos/qos_tracker.py
@@ -345,7 +345,7 @@ class QoSTracker:
                 dp = ryu_switch.dp
                 parser = dp.ofproto_parser
 
-                in_port_no = self.db.get_in_port_no_between_switches(path[i-1], path[i+1], SWITCH_MAP)
+                in_port_no = self.db.get_in_port_no_between_switches(path[i], path[i+1], SWITCH_MAP)
                 in_port = self.db.get_port_for_port_no(in_port_no, path[i].dpid)
 
                 out_port = self.db.get_out_port_no_between_switches(path[i], path[i+1], SWITCH_MAP)
