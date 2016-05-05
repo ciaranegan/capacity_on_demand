a940dc3f54f97b6f63b4da46fa502824cd6751fb
Ciaran Egan <ciaranegan93@gmail.com>
2016-05-05 19:40:36 +0100

    separating QoSTracker from topological logic

--- qos_tracker/qos_tracker.py
+++ qos_tracker/qos_tracker.py
@@ -5,6 +5,7 @@ import time
 import threading
 
 from models import *
+from topology import TopologyManager
 from dbconnection import DBConnection
 from ryu.topology.api import get_all_switch, get_all_link, get_switch
 from ryu.ofproto import ether
@@ -102,6 +103,7 @@ class QoSTracker:
 
     def __init__(self):
         self.db = DBConnection('sqlite:///my_db.db')
+        self.topology_manager(db)
         self._current_mpls_label = 0
         self._flows_added = 0
 
@@ -237,9 +239,9 @@ class QoSTracker:
 
         path = self.get_route_to_host(rsv["dst"], in_switch)
 
-        total_bw = self.get_max_bandwidth_for_path(path)
+        total_bw = self.topology_manager.get_max_bandwidth_for_path(path)
         print "Total Bandwidth: " + str(total_bw)
-        available_bw = self.get_available_bandwidth_for_path(path)
+        available_bw = self.topology_manager.get_available_bandwidth_for_path(path)
 
         if not path or len(path) <= 1:
             return
@@ -326,42 +328,3 @@ class QoSTracker:
         self.add_egress_port_queue(switch, out_port_no, queues, max_bw)
 
         self.add_packet_checking_flow(switch)
-
-    def get_max_bandwidth_for_path(self, path):
-        # TODO: doesn't work for smaller paths
-        bw = None
-        if len(path) > 2:
-            prev_switch = path[0]
-            for i in range(1, len(path)):
-                link = self.db.get_link_between_switches(prev_switch, path[i])
-                if bw is None:
-                    bw = link.bandwidth
-                # Take the smallest as the max reservation can only be as high as the smallest link
-                bw = min(bw, link.bandwidth)
-                prev_switch = path[i]
-        else:
-            print "SHORT PATH, LEN=" + str(len(path))
-
-        return bw
-
-    def get_available_bandwidth_for_path(self, path):
-        # TODO: doesn't work for smaller paths
-        total_bw = self.get_max_bandwidth_for_path(path)
-        if len(path) > 2:
-            prev_switch = path[0]
-            avail_link_bw = []
-            for i in range(1, len(path)):
-                link = self.db.get_link_between_switches(prev_switch, path[i])
-                link_bw = link.bandwidth
-                port_reservations = self.db.get_port_reservations_for_link(link, SWITCH_MAP)
-                if port_reservations:
-                    reservations = []
-                    for p in port_reservations:
-                        reservation = self.db.get_reservation_for_id(p.reservation)
-                        reservations.append(reservation)
-                    for r in reservations:
-                        link_bw -= reservation.bw
-                avail_link_bw.append(link_bw)
-                prev_switch = path[i]
-
-        return min(avail_link_bw)
---
+++ qos_tracker/topology.py
@@ -0,0 +1,71 @@
+
+
+# Mapping of links to port_nos and their bandwidth
+SWITCH_MAP = {
+    s0_DPID: { # DPID: 16
+        3: {
+            "dpid": s2_DPID,
+            "bw": 1000000
+        }
+    },
+    s1_DPID: { # DPID: 32
+        3: {
+            "dpid": s2_DPID,
+            "bw": 1000000
+        }
+    },
+    s2_DPID: {
+        1: {
+            "dpid": s0_DPID,
+            "bw": 1000000
+        },
+        2: {
+            "dpid": s1_DPID,
+            "bw": 1000000
+        }
+    }
+}
+
+class TopologyManager:
+
+    def __init__(self, db):
+        self.db = db
+
+    def get_max_bandwidth_for_path(self, path):
+        # TODO: doesn't work for smaller paths
+        bw = None
+        if len(path) > 2:
+            prev_switch = path[0]
+            for i in range(1, len(path)):
+                link = self.db.get_link_between_switches(prev_switch, path[i])
+                if bw is None:
+                    bw = link.bandwidth
+                # Take the smallest as the max reservation can only be as high as the smallest link
+                bw = min(bw, link.bandwidth)
+                prev_switch = path[i]
+        else:
+            print "SHORT PATH, LEN=" + str(len(path))
+
+        return bw
+
+    def get_available_bandwidth_for_path(self, path):
+        # TODO: doesn't work for smaller paths
+        total_bw = self.get_max_bandwidth_for_path(path)
+        if len(path) > 2:
+            prev_switch = path[0]
+            avail_link_bw = []
+            for i in range(1, len(path)):
+                link = self.db.get_link_between_switches(prev_switch, path[i])
+                link_bw = link.bandwidth
+                port_reservations = self.db.get_port_reservations_for_link(link, SWITCH_MAP)
+                if port_reservations:
+                    reservations = []
+                    for p in port_reservations:
+                        reservation = self.db.get_reservation_for_id(p.reservation)
+                        reservations.append(reservation)
+                    for r in reservations:
+                        link_bw -= reservation.bw
+                avail_link_bw.append(link_bw)
+                prev_switch = path[i]
+
+        return min(avail_link_bw)
