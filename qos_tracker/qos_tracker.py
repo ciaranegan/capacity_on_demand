4413e47d9b579df25778ff031a2dd7602480a704
Ciaran Egan <ciaranegan93@gmail.com>
2016-05-06 12:51:14 +0100

    added ryu-interface file, refactored code there, added src & dst port ids to link model and moved topological constants to own file

--- qos_tracker/qos_tracker.py
+++ qos_tracker/qos_tracker.py
@@ -5,6 +5,7 @@ import time
 import threading
 
 from models import *
+from topology_1_constants import *
 from topology import TopologyManager
 from dbconnection import DBConnection
 from ryu.topology.api import get_all_switch, get_all_link, get_switch
@@ -12,170 +13,57 @@ from ryu.ofproto import ether
 from ryu.lib.ip import ipv4_to_bin
 from IPython import embed
 
-# LOCALHOST = "http://0.0.0.0:8080"
-LOCALHOST = "http://localhost:8080"
-CONF_SWITCH_URI = "/v1.0/conf/switches/"
-QOS_QUEUES_URI = "/qos/queue/"
-QOS_RULES_URI = "/qos/rules/"
-
-s0_DPID = "16"
-s1_DPID = "32"
-s2_DPID = "48"
-
-PIR_TABLE_ID = 0
-CIR_TABLE_ID = 1
-FLOW_TABLE_ID = 2
-
-PORT_NAME_STR = "s{}-eth{}"
-
-SWITCH_NUMBER_TABLE = {
-    s0_DPID: 0,
-    s1_DPID: 1,
-    s2_DPID: 2
-}
-
-OVS_LINK_TYPE = "linux-htb"
-
-# Mapping of port numbers to mac addresses
-HOST_MAP = {
-    s0_DPID: {
-        2: {
-            "mac": '00:00:00:00:00:02',
-            "ip": '10.0.0.2'
-        },
-        1: {
-            "mac": '00:00:00:00:00:01',
-            "ip": '10.0.0.1'
-        }
-    },
-    s1_DPID: {
-        1: {
-            "mac": '00:00:00:00:00:03',
-            "ip": '10.0.0.3'
-        },
-        2: {
-            "mac": '00:00:00:00:00:04',
-            "ip": '10.0.0.4'
-        }
-    },
-    s2_DPID: {
-    }
-}
-
-SWITCH_LOOKUP = {
-    s0_DPID: "0000000000000010",
-    s1_DPID: "0000000000000020",
-    s2_DPID: "0000000000000030"
-}
-
-BEST_EFFORT_QUEUE_ID = 0
-HIGH_PRIORITY_QUEUE_ID = 1
-
-# Mapping of links to port_nos and their bandwidth
-SWITCH_MAP = {
-    s0_DPID: { # DPID: 16
-        3: {
-            "dpid": s2_DPID,
-            "bw": 1000000
-        }
-    },
-    s1_DPID: { # DPID: 32
-        3: {
-            "dpid": s2_DPID,
-            "bw": 1000000
-        }
-    },
-    s2_DPID: {
-        1: {
-            "dpid": s0_DPID,
-            "bw": 1000000
-        },
-        2: {
-            "dpid": s1_DPID,
-            "bw": 1000000
-        }
-    }
-}
-
-OVSDB_ADDR = "tcp:127.0.0.1:6632"
 
 class QoSTracker:
 
     def __init__(self):
         self.db = DBConnection('sqlite:///my_db.db')
         self.topology_manager = TopologyManager(self.db)
+        self.ryu = RyuManager(self.db)
         self._current_mpls_label = 0
         self._flows_added = 0
-
-    def get_port_name_for_port_no(self, port_no, dpid):
-        switch_no = str(SWITCH_NUMBER_TABLE[str(dpid)])
-        return PORT_NAME_STR.format(switch_no, port_no)
 
     def start(self):
         self.db.delete_reservations()
         self.db.delete_queues()
         switches = self.db.get_all_switches()
         for switch in switches:
-            self.put_ovsdb_addr(switch.dpid, OVSDB_ADDR)
+            self.ryu.put_ovsdb_addr(switch.dpid, OVSDB_ADDR)
 
-    def add_egress_port_queue(self, switch, port_no, queues, max_bw):
-        switch_id = self.get_switch_id_for_dpid(switch.dpid)
-        port_name = self.get_port_name_for_port_no(port_no, switch.dpid)
-      
-        data = {
-            "port_name": port_name,
-            "type": OVS_LINK_TYPE,
-           # "max_rate": str(max_bw),
-            "queues": queues
-        }
-        url = LOCALHOST + QOS_QUEUES_URI + switch_id
-        request = requests.post(url, data=json.dumps(data))
+    # def add_port_queue(self, switch, port_no, queues):
+    #     switch_id = self.get_switch_id_for_dpid(switch.dpid)
+    #     port_name = self.get_port_name_for_port_no(port_no, switch.dpid)
 
-    def add_port_queue(self, switch, port_no, queues):
-        switch_id = self.get_switch_id_for_dpid(switch.dpid)
-        port_name = self.get_port_name_for_port_no(port_no, switch.dpid)
+    #     for queue in queues:
+    #         if "max_rate" in queue:
+    #             max_rate = queue["max_rate"]
+    #         else:
+    #             max_rate = None
+    #         if "min_rate" in queue:
+    #             min_rate = queue["min_rate"]
+    #         else:
+    #             min_rate = None
 
-        for queue in queues:
-            if "max_rate" in queue:
-                max_rate = queue["max_rate"]
-            else:
-                max_rate = None
-            if "min_rate" in queue:
-                min_rate = queue["min_rate"]
-            else:
-                min_rate = None
+    #         data = {
+    #             "port_name": port_name,
+    #             "type": OVS_LINK_TYPE,
+    #             "max_rate": str(max_rate),
+    #             "queues": queues
+    #         }
 
-            data = {
-                "port_name": port_name,
-                "type": OVS_LINK_TYPE,
-                "max_rate": str(max_rate),
-                "queues": queues
-            }
+    #         url = LOCALHOST + QOS_QUEUES_URI + switch_id
+    #         request = requests.post(url, data=json.dumps(data))
+    #         print "Request returned(port_queue_init): " + str(request.text)
 
-            url = LOCALHOST + QOS_QUEUES_URI + switch_id
-            request = requests.post(url, data=json.dumps(data))
-            print "Request returned(port_queue_init): " + str(request.text)
+    # def get_max_bw_for_topo(self):
+    #     links = self.db.get_all_links()
+    #     max_bw = 0
+    #     for link in links:
+    #         max_bw = max(max_bw, link.bandwidth)
+    #     return max_bw
 
-    def get_max_bw_for_topo(self):
-        links = self.db.get_all_links()
-        max_bw = 0
-        for link in links:
-            max_bw = max(max_bw, link.bandwidth)
-        return max_bw
-
-    def put_ovsdb_addr(self, dpid, ovsdb_addr):
-        switch_id = self.get_switch_id_for_dpid(dpid)
-        url = LOCALHOST + CONF_SWITCH_URI + switch_id + "/ovsdb_addr"
-        r = requests.put(url, data=json.dumps(ovsdb_addr))
-
-    def get_switch_id_for_dpid(self, dpid):
-        return SWITCH_LOOKUP[str(dpid)]
-
-    def get_reservation_for_src_dst(self, src, dst):
-        return self.db.get_reservation_for_src_dst(src, dst)
-
-    def get_switch_for_dpid(self, dpid):
-        return self.db.get_switch_for_dpid(dpid)
+    # def get_reservation_for_src_dst(self, src, dst):
+    #     return self.db.get_reservation_for_src_dst(src, dst)
 
     def generate_mpls_label(self):
         self._current_mpls_label += 1
@@ -240,65 +128,34 @@ class QoSTracker:
         out_port = self.db.get_port_for_id(reservation.out_port)
         self.add_switch_rules(path[-1], out_port.port_no, reservation.src, reservation.dst, reservation.bw, total_bw)
 
-    def add_queue_flow(self, switch, port, src, dst, queue_id=HIGH_PRIORITY_QUEUE_ID):
-        switch_id = self.get_switch_id_for_dpid(switch.dpid)
-        data = {
-            "match": {
-                "nw_dst": dst,
-                "nw_src": src,
-                "nw_proto": "UDP"
-        #        "dl_type": "IPv4"
-            },
-            "actions": {
-                "queue": queue_id
-            }
-        }
-        url = LOCALHOST + QOS_RULES_URI + switch_id
-        request = requests.post(url, data=json.dumps(data))
-        print "Request returned(queue_init): " + str(request.text)
-
-    def add_packet_checking_flow(self, switch):
-        switch_id = self.get_switch_id_for_dpid(switch.dpid)
-        data = {
-            "match": {
-                "ip_dscp": 26
-            },
-            "actions": {
-                "queue": 1
-            }
-        }
-        url = LOCALHOST + QOS_RULES_URI + switch_id
-        request = requests.post(url, data=json.dumps(data))
-        print "Packet checking request returned: " + str(request.text)
-
-    def add_packet_marking_flow(self, switch, src, dst):
-        switch_id = self.get_switch_id_for_dpid(switch.dpid)
-        data = {
-            "match": {
-                "nw_dst": dst,
-                "nw_src": src,
-                "nw_proto": "UDP"
-            },
-            "actions": {
-                "mark": 26,
-                "queue": 1
-            }
-        }
-        url = LOCALHOST + QOS_RULES_URI + switch_id
-        request = requests.post(url, data=json.dumps(data))
-        print "Packet marking request returned: " + str(request.text)
+    # def add_queue_flow(self, switch, port, src, dst, queue_id=HIGH_PRIORITY_QUEUE_ID):
+    #     switch_id = self.get_switch_id_for_dpid(switch.dpid)
+    #     data = {
+    #         "match": {
+    #             "nw_dst": dst,
+    #             "nw_src": src,
+    #             "nw_proto": "UDP"
+    #     #        "dl_type": "IPv4"
+    #         },
+    #         "actions": {
+    #             "queue": queue_id
+    #         }
+    #     }
+    #     url = LOCALHOST + QOS_RULES_URI + switch_id
+    #     request = requests.post(url, data=json.dumps(data))
+    #     print "Request returned(queue_init): " + str(request.text)
 
     def add_ingress_rules(self, switch, out_port_no, src_ip, dst_ip, bw, max_bw):
         # Add queues
         queues = [{"max_rate": str(max_bw)}, {"min_rate": str(bw)}]
-        self.add_egress_port_queue(switch, out_port_no, queues, max_bw)
+        self.ryu.add_egress_port_queue(switch, out_port_no, queues, max_bw)
 
         # Mark the packets on their way in
-        self.add_packet_marking_flow(switch, src_ip, dst_ip)
+        self.ryu.add_packet_marking_flow(switch, src_ip, dst_ip)
 
     def add_switch_rules(self, switch, out_port_no, src_ip, dst_ip, bw, max_bw):
         # Add queues
         queues = [{"max_rate": str(max_bw)}, {"min_rate": str(bw)}]
-        self.add_egress_port_queue(switch, out_port_no, queues, max_bw)
+        self.ryu.add_egress_port_queue(switch, out_port_no, queues, max_bw)
 
-        self.add_packet_checking_flow(switch)
+        self.ryu.add_packet_checking_flow(switch)
---
+++ qos_tracker/ryu.py
@@ -0,0 +1,80 @@
+import requests
+import json
+
+import topology_1_constants
+
+LOCALHOST = "http://localhost:8080"
+CONF_SWITCH_URI = "/v1.0/conf/switches/"
+QOS_QUEUES_URI = "/qos/queue/"
+QOS_RULES_URI = "/qos/rules/"
+
+OVSDB_ADDR = "tcp:127.0.0.1:6632"
+
+OVS_LINK_TYPE = "linux-htb"
+
+PORT_NAME_STR = "s{}-eth{}"
+
+class RyuManager:
+
+    def __init__(self, db):
+        print "hello ryu_manager"
+        self.db = db
+
+    # def get_switch_for_dpid(self, dpid):
+    #     return self.db.get_switch_for_dpid(dpid)
+
+    def get_switch_id_for_dpid(self, dpid):
+        return SWITCH_LOOKUP[str(dpid)]
+
+    def add_packet_checking_flow(self, switch):
+        switch_id = self.get_switch_id_for_dpid(switch.dpid)
+        data = {
+            "match": {
+                "ip_dscp": 26
+            },
+            "actions": {
+                "queue": 1
+            }
+        }
+        url = LOCALHOST + QOS_RULES_URI + switch_id
+        request = requests.post(url, data=json.dumps(data))
+        print "Packet checking request returned: " + str(request.text)
+
+    def add_packet_marking_flow(self, switch, src, dst):
+        switch_id = self.get_switch_id_for_dpid(switch.dpid)
+        data = {
+            "match": {
+                "nw_dst": dst,
+                "nw_src": src,
+                "nw_proto": "UDP"
+            },
+            "actions": {
+                "mark": 26,
+                "queue": 1
+            }
+        }
+        url = LOCALHOST + QOS_RULES_URI + switch_id
+        request = requests.post(url, data=json.dumps(data))
+        print "Packet marking request returned: " + str(request.text)
+
+    def add_egress_port_queue(self, switch, port_no, queues, max_bw):
+        switch_id = self.get_switch_id_for_dpid(switch.dpid)
+        port_name = self.get_port_name_for_port_no(port_no, switch.dpid)
+      
+        data = {
+            "port_name": port_name,
+            "type": OVS_LINK_TYPE,
+           # "max_rate": str(max_bw),
+            "queues": queues
+        }
+        url = LOCALHOST + QOS_QUEUES_URI + switch_id
+        request = requests.post(url, data=json.dumps(data))
+
+    def put_ovsdb_addr(self, dpid, ovsdb_addr):
+        switch_id = self.get_switch_id_for_dpid(dpid)
+        url = LOCALHOST + CONF_SWITCH_URI + switch_id + "/ovsdb_addr"
+        r = requests.put(url, data=json.dumps(ovsdb_addr))
+
+    def get_port_name_for_port_no(self, port_no, dpid):
+        switch_no = str(SWITCH_NUMBER_TABLE[str(dpid)])
+        return PORT_NAME_STR.format(switch_no, port_no)
